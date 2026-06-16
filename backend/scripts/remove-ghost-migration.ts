226e04dbae27bf2ef619cf6ae2f69551abe9d0c6
Xuan Thang <Thang@gmail.com>
2026-06-16 23:57:50 +0700

    chore: remove temporary migration debug scripts

--- backend/scripts/remove-ghost-migration.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../src/libs/database.ts"
-
-await prisma.$executeRaw`
-  DELETE FROM "_prisma_migrations"
-  WHERE migration_name = '20260616112539_okok'
-`
-console.log("Removed ghost migration 20260616112539_okok")
-await prisma.$disconnect()
