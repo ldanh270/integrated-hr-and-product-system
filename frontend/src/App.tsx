d643516ce4db5d64810d2ce312fee85ece727ac0
ldanh270 <ducanhle.dn@gmail.com>
2026-07-01 22:34:46 +0700

    fix: fix seeder errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -78,7 +78,7 @@ const ProtectedRoute = ({
   }
 
   if (requiredPermissions && user) {
-    const hasPermission = requiredPermissions.every((p) => user.permissions.includes(p))
+    const hasPermission = requiredPermissions.every((p) => user.permissions?.includes(p))
     if (!hasPermission) {
       return <Navigate to={ROUTES.HRM.DASHBOARD} replace />
     }
